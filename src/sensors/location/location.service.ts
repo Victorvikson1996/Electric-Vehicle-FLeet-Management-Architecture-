6bc20f146fd3e38c02e036facc7724d0c9a5a848
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2025-02-23 00:21:10 +0100

    git commit -m Added nodes folder with fleetManager, chargingStation, and vehicle services

--- src/sensors/location/location.service.ts
+++ src/sensors/location/location.service.ts
@@ -1,4 +1,21 @@
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class LocationService {}
+
 import { Injectable } from '@nestjs/common';
+// import { GpsDriver } from '../drivers/gps.driver';
+import { GpsDriver } from 'src/drivers/gps/gps.service';
 
 @Injectable()
-export class LocationService {}
+export class LocationSensor {
+  constructor(private gpsDriver: GpsDriver) {}
+
+  public getLocation(): { lat: number; lng: number } {
+    return this.gpsDriver.getLocation();
+  }
+
+  public updateLocation(lat: number, lng: number): void {
+    this.gpsDriver.updateLocation(lat, lng);
+  }
+}
