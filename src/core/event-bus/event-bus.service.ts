ab4e9973d5df6282ffded1b6e5a6dff7434dfe1a
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2025-02-22 21:23:21 +0100

    event bus

--- src/core/event-bus/event-bus.service.ts
+++ src/core/event-bus/event-bus.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { EventEmitter2 } from 'eventemitter2';
 
 @Injectable()
-export class EventBusService {}
+export class EventBusService {
+  constructor(private eventEmitter: EventEmitter2) {}
+
+  public publish<T>(event: string, data: T): void {
+    this.eventEmitter.emit(event, data);
+  }
+
+  public subscribe<T>(event: string, callback: (data: T) => void): void {
+    this.eventEmitter.on(event, callback);
+  }
+
+  public unsubscribe<T>(event: string, callback: (data: T) => void): void {
+    this.eventEmitter.off(event, callback);
+  }
+}
