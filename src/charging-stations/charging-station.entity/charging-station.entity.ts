0f15dec0cb69f22b290c61ff01c1bb1eb9c0814d
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2025-02-23 02:00:01 +0100

    Charging-station

--- src/charging-stations/charging-station.entity/charging-station.entity.ts
+++ src/charging-stations/charging-station.entity/charging-station.entity.ts
@@ -1,1 +1,16 @@
-export class ChargingStationEntity {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class ChargingStation {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('jsonb')
+  location: { lat: number; lng: number };
+
+  @Column()
+  isAvailable: boolean;
+
+  @Column()
+  lastUpdated: Date;
+}
