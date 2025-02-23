e9d3d7f006c6ef4c9a871f37e7906c261199f6e6
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2025-02-23 01:59:03 +0100

    Auth and Vehicles controllers, entities fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserService } from './user/user.service';
+// import { UserService } from './user/user.service';
 import { JwtStrategy } from './jwt-strategy/jwt-strategy';
+import { User } from './user/user.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService, JwtStrategy],
+  // providers: [AuthService, UserService, JwtStrategy],
+  providers: [AuthService, User, JwtStrategy],
 })
 export class AuthModule {}
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,4 +1,31 @@
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class VehiclesService {}
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Vehicle } from './vehicle.entity/vehicle.entity';
 
 @Injectable()
-export class VehiclesService {}
+export class VehicleService {
+  constructor(
+    @InjectRepository(Vehicle)
+    private vehiclesRepository: Repository<Vehicle>,
+  ) {}
+
+  async create(vehicle: Partial<Vehicle>): Promise<Vehicle> {
+    const newVehicle = this.vehiclesRepository.create(vehicle);
+    return await this.vehiclesRepository.save(newVehicle);
+  }
+
+  async findAll(): Promise<Vehicle[]> {
+    return await this.vehiclesRepository.find();
+  }
+
+  async update(id: string, vehicle: Partial<Vehicle>): Promise<Vehicle> {
+    await this.vehiclesRepository.update(id, vehicle);
+    return await this.vehiclesRepository.findOneOrFail({ where: { id } });
+  }
+}
