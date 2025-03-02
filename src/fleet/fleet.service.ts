96c293130081d409f52f509016f85f64a5e0033e
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2025-03-02 23:42:34 +0100

    Fleet service error not fixed yet

--- src/fleet/fleet.service.ts
+++ src/fleet/fleet.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FleetService {}
+export class FleetService {
+  getFleet(): string {
+    return 'This action returns all fleet';
+  }
+
+  getFleetById(id: number): string {
+    return `This action returns a #${id} fleet`;
+  }
+
+  createFleet(): string {
+    return 'This action adds a new fleet';
+  }
+
+  updateFleet(id: number): string {
+    return `This action updates a #${id} fleet`;
+  }
+
+  deleteFleet(id: number): string {
+    return `This action removes a #${id} fleet`;
+  }
+}
