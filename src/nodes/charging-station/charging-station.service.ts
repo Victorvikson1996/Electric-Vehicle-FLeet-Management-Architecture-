ffa65a1b376e2ad2cf7510ed796cbac59a50467d
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2025-03-02 23:33:34 +0100

    Fleet Services Error

--- src/nodes/charging-station/charging-station.service.ts
+++ src/nodes/charging-station/charging-station.service.ts
@@ -1,4 +1,34 @@
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class ChargingStationService {}
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ChargingStation } from 'src/charging-stations/charging-station.entity/charging-station.entity';
 
 @Injectable()
-export class ChargingStationService {}
+export class ChargingStationService {
+  constructor(
+    @InjectRepository(ChargingStation)
+    private stationsRepository: Repository<ChargingStation>,
+  ) {}
+
+  async create(station: Partial<ChargingStation>): Promise<ChargingStation> {
+    const newStation = this.stationsRepository.create(station);
+    return await this.stationsRepository.save(newStation);
+  }
+
+  async findAll(): Promise<ChargingStation[]> {
+    return await this.stationsRepository.find();
+  }
+
+  async update(
+    id: string,
+    station: Partial<ChargingStation>,
+  ): Promise<ChargingStation> {
+    await this.stationsRepository.update(id, station);
+    return await this.stationsRepository.findOneOrFail({ where: { id } });
+  }
+}
